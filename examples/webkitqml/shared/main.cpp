6ac96bfa69342401789477edb20b52fdf64381c5
Thiago Macieira <thiago.macieira@intel.com>
2012-12-18 13:03:29 -0800

    Compile in C++11 mode: spaces between string literal concatenations
    Or we get the error:
    main.cpp:46:118: error: unable to find string literal operator 'operator"" QWEBKIT_EXAMPLE_NAME'
    
    Change-Id: Ic13fa250329f18f9b847468764bd00258df34c7d
    Reviewed-by: Simon Hausmann <simon.hausmann@digia.com>

--- examples/webkitqml/shared/main.cpp
+++ examples/webkitqml/shared/main.cpp
@@ -43,7 +43,7 @@ int main(int argc, char* argv[])
 {
     QGuiApplication app(argc,argv);
     QQuickView view;
-    view.setSource(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() + QLatin1String("/"QWEBKIT_EXAMPLE_NAME".qml")));
+    view.setSource(QUrl::fromLocalFile(QCoreApplication::applicationDirPath() + QLatin1String("/" QWEBKIT_EXAMPLE_NAME ".qml")));
     view.setResizeMode(QQuickView::SizeRootObjectToView);
     view.show();
     return app.exec();
