734663277730bc20dacf0a6b07427002a53cc107
Sergio Ahumada <sahumada@blackberry.com>
2015-03-04 22:56:29 +0100

    Update links from qt-project.org to qt.io
    - remove http://qt.digia.com since it now points to qt.io
    - move webkit.org down to match other repos
    
    Change-Id: I036b3ed84bdd8023fd32b7e9ccfde787ce666939
    Reviewed-by: Venugopal Shivashankar <venugopal.shivashankar@digia.com>

--- examples/webkitwidgets/embedded/anomaly/src/BookmarksView.cpp
+++ examples/webkitwidgets/embedded/anomaly/src/BookmarksView.cpp
@@ -46,7 +46,7 @@ BookmarksView::BookmarksView(QWidget *parent)
     layout->addWidget(m_iconView);
 
     m_iconView->addItem("www.google.com");
-    m_iconView->addItem("qt-project.org/doc/qt-5.0");
+    m_iconView->addItem("doc.qt.io/qt-5");
     m_iconView->addItem("news.bbc.co.uk/2/mobile/default.stm");
     m_iconView->addItem("mobile.wikipedia.org");
     m_iconView->addItem("qt.digia.com");
